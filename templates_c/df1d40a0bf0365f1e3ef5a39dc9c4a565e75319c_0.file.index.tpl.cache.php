e3fcd028796b4e62811afd44421ed66a2cb3235e
MachelleBa <613954@student.inholland.nl>
2018-11-15 16:37:56 +0100

    appearantly there still were unpushed things, here they are

---
+++ templates_c/df1d40a0bf0365f1e3ef5a39dc9c4a565e75319c_0.file.index.tpl.cache.php
@@ -0,0 +1,81 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-11-15 16:30:58
+  from 'C:\xampp\htdocs\templates\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bed9132d8e8e5_26238331',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'df1d40a0bf0365f1e3ef5a39dc9c4a565e75319c' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\templates\\index.tpl',
+      1 => 1542211782,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:header.tpl' => 1,
+    'file:footer.tpl' => 1,
+  ),
+),false)) {
+function content_5bed9132d8e8e5_26238331 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '10746780425bed9132c31c45_91283986';
+$_smarty_tpl->smarty->ext->configLoad->_loadConfigFile($_smarty_tpl, "test.conf", "setup", 0);
+?>
+
+<?php $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 9999, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+<body>
+
+<!-- SLIDESHARE--> 
+<h1>
+<figure id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <figure class="carousel-inner">
+    <figure class="carousel-item active">
+      <img class="d-block w-100" src="./templates/img/example.jpg" alt="First slide">
+    </figure>
+    <figure class="carousel-item">
+      <img class="d-block w-100" src="./templates/img/example.jpg" alt="Second slide">
+    </figure>
+    <figure class="carousel-item">
+      <img class="d-block w-100" src="./templates/img/example.jpg" alt="Third slide">
+    </figure>
+  </figure>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</figure></h1>
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<?php echo '<script'; ?>
+ src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+</body>
+</body>
+
+<?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 9999, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
